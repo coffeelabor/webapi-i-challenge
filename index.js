a121e60c85fb5ac16c5703a9a35983eadc557cef
Reed James <reedtjames1234@gmail.com>
2019-08-05 14:02:19 -0500

    Initial commit, server initialized, GET /users complete

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // implement your API here
+const express = require("express");
+
+const Users = require("./data/db.js");
+
+const server = express();
+
+server.use(express.json());
+
+server.get("/", (req, res) => {
+  res.send("howdy");
+});
+
+server.get("/users", (req, res) => {
+  Users.find()
+    .then(user => {
+      res.status(200).json(user);
+    })
+    .catch(error => {
+      res
+        .status(500)
+        .json({ message: "Bad news bruh, you cannot GET that array" });
+    });
+});
+
+const port = 8000;
+server.listen(port, () => console.log("api is feelin like a million bucks"));
